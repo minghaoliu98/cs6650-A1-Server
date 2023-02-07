15e9e1d1e8b5b95ff81e525a1950464cec0b1c53
Minghao Liu <lmh98@uw.edu>
2023-02-07 10:59:35 -0800

    v2

--- A1-server/src/main/java/SkierServlet.java
+++ A1-server/src/main/java/SkierServlet.java
@@ -22,10 +22,45 @@ public class SkierServlet extends HttpServlet {
         String jsonString = ReadBigStringIn(req.getReader());
         JsonObject convertedObject = new Gson().fromJson(jsonString, JsonObject.class);
         res.setContentType("text/plain");
-        res.setStatus(HttpServletResponse.SC_OK);
-        res.getWriter().write(convertedObject.toString());
+        res.getWriter().write(validation(convertedObject));
+        if (!(validation(convertedObject)).equals("true")) {
+            res.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            res.setStatus(400);
+        }
     }
 
+
+    public String validation(JsonObject json) {
+        try {
+            String swipe = json.get("swipe").getAsString();
+            if (!swipe.equals("Left") && !swipe.equals("Right")) {
+                return "swipe";
+            }
+            if (json.get("swiper").getAsInt() < 1 || json.get("swiper").getAsInt() > 5000) {
+                return "swiper";
+            }
+            if (json.get("swipee").getAsInt() < 1 || json.get("swipee").getAsInt() > 1000000) {
+                return "swipee";
+            }
+            String comment = json.get("comment").getAsString();
+            if (comment.length() != 256) {
+                return "comment";
+            }
+            for(int i = 0; i < comment.length(); i++) {
+                if (!Character.isLetter(comment.charAt(i))) {
+                    return "comment";
+                }
+            }
+
+        } catch (Exception e) {
+            return "O";
+        }
+
+
+
+        return "true";
+    }
     public String ReadBigStringIn(BufferedReader buffIn) throws IOException {
         StringBuilder everything = new StringBuilder();
         String line;
